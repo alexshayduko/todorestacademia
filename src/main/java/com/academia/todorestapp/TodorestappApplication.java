dafe74993cb531d37e9163cdd551d8bd07762a62
Indarello <46714497+Indarello@users.noreply.github.com>
2020-10-16 11:31:30 +0700

    First upload blank project

---
+++ src/main/java/com/academia/todorestapp/TodorestappApplication.java
@@ -0,0 +1,13 @@
+package com.academia.todorestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodorestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodorestappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academia/todorestapp/TodorestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.todorestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodorestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
